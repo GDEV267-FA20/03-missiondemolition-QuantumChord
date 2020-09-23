b53491f675ae77daa87aad45227791e59760ac27
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2020-09-23 15:35:15 -0400

    Projectile Trail

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour{
 
+    static private Slingshot S;
+
     //fields set in Unity Inspector pane
 
     [Header(("Set in Inspector"))]
@@ -26,8 +28,20 @@ public class Slingshot : MonoBehaviour{
 
     private Rigidbody  projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
+        S = this;
+
         Transform launchPointTrans = transform.Find("LaunchPoint");
 
         launchPoint = launchPointTrans.gameObject;
